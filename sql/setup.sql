f2aa982919f398c4b2b27a16d468dcff07972ce4
Larissa Galan <jarbiedeleon123456789@example.com>
2026-09-03 04:10:15 +0800

    Derive member contacts from names

--- sql/setup.sql
+++ sql/setup.sql
@@ -15,10 +15,10 @@ CREATE TABLE IF NOT EXISTS users (
 
 INSERT INTO users (firstname, lastname, email, username)
 VALUES
-('Larissa', 'Galan', 'jarbie@example.com', 'dyarbe'),
-('Sabina', 'Elumba', 'james@example.com', 'dyems'),
-('Jeorge', 'Slaying', 'dylan@example.com', 'Dylan'),
-('Wasabi', 'Na', 'Roi@example.com', 'balong'),
-('Issa', 'Milley', 'russel@example.com', 'russel');
+('Larissa', 'Galan', 'larissa.galan@example.com', 'larissagalan'),
+('Sabina', 'Elumba', 'sabina.elumba@example.com', 'sabinaelumba'),
+('Jeorge', 'Slaying', 'jeorge.slaying@example.com', 'jeorgeslaying'),
+('Wasabi', 'Na', 'wasabi.na@example.com', 'wasabina'),
+('Issa', 'Milley', 'issa.milley@example.com', 'issamilley');
 
 SELECT * FROM users;
